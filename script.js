6d2677e33429d2802e8f708ca1fb639c046c0b58
Jair Hernández <startupstijuana@gmail.com>
2023-02-13 16:58:45 -0800

    Update
    Se agrego nombre de empresa y nombre y apellido de quien llena el formulario

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 function sendData() {
+  companyName = document.getElementById("companyName").value;
+  staffName = document.getElementById("staffName").value;
   q1o1 = parseInt(document.getElementById("q1o1").value);
   q1o2 = parseInt(document.getElementById("q1o2").value);
   q1o3 = parseInt(document.getElementById("q1o3").value);
@@ -19,10 +21,12 @@ function sendData() {
   q8o1 = parseInt(document.getElementById("q8o1").value);
   q8o2 = parseInt(document.getElementById("q8o2").value);
   q8o3 = parseInt(document.getElementById("q8o3").value);
-  console.log(q1o1, q1o2,q1o3, q2o1, q2o2, q2o3, q3o1, q3o2, q3o3, q4o1, q5o1, q5o2,q6o1,q6o2, q7o1, q7o2,q8o1, q8o2, q8o3);
+  console.log(companyName, staffName,q1o1, q1o2,q1o3, q2o1, q2o2, q2o3, q3o1, q3o2, q3o3, q4o1, q5o1, q5o2,q6o1,q6o2, q7o1, q7o2,q8o1, q8o2, q8o3);
   Swal.fire('Enviando información')
 
   var data = JSON.stringify({
+    "companyName": `${companyName}`,
+    "staffName": `${staffName}`,    
     "q1o1": `${q1o1}`,
     "q1o2": `${q1o2}`,
     "q1o3": `${q1o3}`,
